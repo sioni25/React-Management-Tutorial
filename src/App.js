7e7b751c5a0463aab5b45f4cf586e2f81dd530cd
sioni25 <ktldhs24@gmail.com>
2025-02-20 19:52:43 +0900

    test

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="gray-background">
+        기본적으로 리액트에서는 JSX문법을 사용함sdfsfdd
+        <img src={logo} let="logo" />
+    </div>
+  );
+}
+
+export default App;
